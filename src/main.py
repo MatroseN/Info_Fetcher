189ff502540c2cf148cdc1a3fa5a7bdfad11d1a1
a17fellu <felix.lundh@gmail.com>
2021-04-14 15:20:33 +0200

    utf-8

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ directory = 'TestDir'
 
 # Read text File
 def read_text_file(file_path):
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r', encoding="utf-8") as f:
         print(f.read())
 
 
