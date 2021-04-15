1970c9d59420c4139753bd8a0d7b7ebb5d5c387f
a17fellu <felix.lundh@gmail.com>
2021-04-15 21:13:11 +0200

    boxplot update

--- src/boxplot_builder.py
+++ src/boxplot_builder.py
@@ -20,15 +20,21 @@ except IOError:
 batch_size_8 = np.array(model_dict.get("Batch_size: 8")).astype(float)
 batch_size_16 = np.array(model_dict.get("Batch_size: 16")).astype(float)
 batch_size_32 = np.array(model_dict.get("Batch_size: 32")).astype(float)
+batch_size_64 = np.array(model_dict.get("Batch_size: 64")).astype(float)
+batch_size_128 = np.array(model_dict.get("Batch_size: 128")).astype(float)
 
 
-columns = [batch_size_8, batch_size_16, batch_size_32]
+columns = [batch_size_8, batch_size_16, batch_size_32, batch_size_64, batch_size_128]
 
 fig = plt.figure(figsize=(10, 7))
 
 # Creating plot
+# Add title and axis names
+plt.title('Batch size')
+plt.xlabel('Models')
+plt.ylabel('Test accuracy %')
 plt.boxplot(columns)
-plt.xticks([1, 2, 3], ['Batch_size:8', 'Batch_size:16', 'Batch_size:32'])
+plt.xticks([1, 2, 3, 4, 5], ['Batch_size:8', 'Batch_size:16', 'Batch_size:32', 'Batch_size:64', 'Batch_size:128'])
 
 # show plot
 plt.show()
